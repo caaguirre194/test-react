58c9064041d7677784a314cee0345bb38ab7be26
CarlosAguirre-SB <carlos.aguirre@segurosbolivar.com>
2021-04-24 14:47:28 -0500

    feat: Base project was created.

---
+++ inmunoderm-front-react/src/components/App/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Preferences from "../Preferences/Preferences";
+
+function App() {
+    return (
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/preferences">
+                        <Preferences />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
